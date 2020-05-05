86bb105bae8b4966ad168a0037f8551db0be2105
jreadey <jreadey@hdfgroup.org>
2020-05-05 09:01:19 -0700

    use symbolic links for lambda files

--- awslambda/build.sh
+++ awslambda/build.sh
@@ -39,13 +39,6 @@ if [ $dolint ]; then
 fi
 
 
-# copy util files that are the same for lambda
-SRC=../hsds/util
-DES=chunkread/hsds/util
-cp $SRC/arrayUtil.py $DES
-cp $SRC/hdf5dtype.py $DES
-cp $SRC/domainUtil.py $DES
-
 zip ${ZIPFILE} chunkread/lambda_function.py
 zip ${ZIPFILE} chunkread/__init__.py
 zip ${ZIPFILE} chunkread/hsds/*.py
---
+++ awslambda/chunkread/hsds/util/hdf5dtype.py
@@ -0,0 +1,1 @@
+../../../../hsds/util/hdf5dtype.py
