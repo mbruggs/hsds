04d524b7d8f5d605ad025476f262740e0f498b27
jreadey <jreadey@hdfgroup.org>
2021-10-10 23:46:14 -0700

    add max_tasks option

--- tests/perf/nsrdb/nsrdb_async.py
+++ tests/perf/nsrdb/nsrdb_async.py
@@ -16,11 +16,13 @@ NUM_COLS = 17568
 NUM_ROWS = 2018392
 DSET_ID = "d-096b7930-5dc5b556-dbc8-00c5ad-8aca89"  # wind-speed dataset
 DSET_TYPE = 'i2'  # two-byte signed integer
+NUM_RETRIES = 10
+SLEEP_TIME = 0.1
 
 cfg = Config()
 
 class DataFetcher:
-    def __init__(self, app, max_tasks=100):
+    def __init__(self, app, max_tasks=10):
         self._app = app
           
         logging.info(f"DataFetcher.__init__  {self.bucket}/{self.domain} {self.dsetid}")
@@ -100,6 +102,14 @@ class DataFetcher:
     @property
     def result(self):
         return self._app["result"]
+
+    @property
+    def retries(self):
+        return self._app["retries"]
+
+    @property
+    def sleep_time(self):
+        return self._app["sleep_time"]
          
 
     async def fetch(self):
@@ -117,18 +127,42 @@ class DataFetcher:
         logging.debug("DataFetcher - workers canceled")
 
     async def work(self):
+        max_retries = self.retries
         async with aiohttp.ClientSession() as session:
+            block = await self._q.get()
+            retry_count = 0
+            sleep_time = self.sleep_time
+
             while True:
                 start_ts = time.time()
-                block = await self._q.get()
+                status_code = None
                 try:
-                    await self.read_block(session, block)
+                    status_code = await self.read_block(session, block)
                 except IOError as ioe:
                     logging.error(f"got IOError: {ioe}")
-                self._q.task_done()
                 elapsed = time.time() - start_ts
                 msg = f"DataFetcher - task {block} start: {start_ts:.3f} "
                 msg += f"elapsed: {elapsed:.3f}"
+                if status_code == 200:
+                    self._q.task_done()
+                    block = await self._q.get()
+                    retry_count = 0
+                    sleep_time = self.sleep_time
+
+                elif status_code == 503:
+                    logging.warning(f"server too busy, sleeping for {sleep_time}")
+                    await asyncio.sleep(sleep_time)
+                    sleep_time *= 2  # wait twice as long next time
+                else:
+                    logging.error(f"got status code: {status_code} retry: {retry_count}")
+                    retry_count += 1
+                    if retry_count > max_retries:
+                        # move on to another block
+                        self._q.task_done()
+                        block = await self._q.get()
+                        retry_count = 0
+                        sleep_time = self.sleep_time
+
                 logging.info(msg)
 
     async def read_block(self, session, block):
@@ -150,6 +184,7 @@ class DataFetcher:
         params["domain"] = self.domain
         params["bucket"] = self.bucket
         logging.debug(f"read_block({block}): sending req: {req}, {select}")
+        status_code = 500
         async with session.get(req, headers=headers, params=params) as rsp:
             if rsp.status == 200:
                 if 'Content-Type' not in rsp.headers:
@@ -170,17 +205,27 @@ class DataFetcher:
                 result = self.result
                 # slot in to result array
                 result[row_start:row_end] = arr
+            status_code =rsp.status
+        return status_code
+        
        
 
 # parse command line args
 index = None
 block_size = None
+max_tasks = 10
 for narg in range(1, len(sys.argv)):
     arg = sys.argv[narg]
+    if arg in ("-h", "--help"):
+        print("usage: python nsrdb_async.py [--index=n] [--block==n] [--tasks=n]")
+        sys.exit(0)
     if arg.startswith("--index="):
         index = int(arg[len("--index="):])
     elif arg.startswith("--block="):
         block_size = int(arg[len("--block="):])
+    elif arg.startswith("--tasks="):
+        max_tasks = int(arg[len("--tasks="):])
+    
     else:
         print(f"unexpected argument: {arg}")
  
@@ -191,7 +236,7 @@ if block_size is None:
     # read entire column in one call
     block_size = DEFAULT_BLOCK_SIZE
 
-loglevel = logging.ERROR
+loglevel = logging.WARNING
 logging.basicConfig(format='%(asctime)s %(message)s', level=loglevel)
     
 # init app dictionary
@@ -203,13 +248,15 @@ cfg["bucket_name"] = BUCKET_NAME
 cfg["block_size"] = block_size
 cfg["index"] = index
 cfg["num_rows"] = NUM_ROWS
+cfg["retries"] =  NUM_RETRIES
+cfg["sleep_time"] = SLEEP_TIME
 
 # array will be filled in by workers
 result = np.zeros((NUM_ROWS,), dtype=np.dtype(DSET_TYPE))
 
 cfg["result"] = result
 
-data_fetcher = DataFetcher(cfg)
+data_fetcher = DataFetcher(cfg, max_tasks=max_tasks)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(data_fetcher.fetch())
 print(f"{H5_PATH}[{index}:]: {result}")
